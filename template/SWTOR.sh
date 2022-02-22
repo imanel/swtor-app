204a70caf1895c0798eddda294bd84a187560c66
Bernard Potocki <bernard.potocki@imanel.org>
2022-02-22 22:23:14 +0100

    Use absolute path to wine in loader + add logs to Console

--- template/SWTOR.sh
+++ template/SWTOR.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 PREFIX_DIR="$SCRIPT_DIR/../Resources/prefix"
 SWTOR_LAUNCHER_DIR="$PREFIX_DIR/drive_c/Program Files/Electronic Arts/BioWare/Star Wars - The Old Republic/"
 
-WINEPREFIX=$PREFIX_DIR wine "$SWTOR_LAUNCHER_DIR/launcher.exe"
+WINEPREFIX=$PREFIX_DIR /opt/homebrew/bin/wine "$SWTOR_LAUNCHER_DIR/launcher.exe" > ~/Library/Logs/SWTOR.log 2>&1
