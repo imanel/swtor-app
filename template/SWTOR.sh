ac83f17428af20ca836e5381f1fd0f5361410557
Bernard Potocki <bernard.potocki@imanel.org>
2022-02-22 21:48:48 +0100

    Simplify launcher file

--- template/SWTOR.sh
+++ template/SWTOR.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env arch -x86_64 bash
 
-export PATH="/usr/local/bin:$PATH"
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+PREFIX_DIR="$SCRIPT_DIR/../Resources/prefix"
+SWTOR_LAUNCHER_DIR="$PREFIX_DIR/drive_c/Program Files/Electronic Arts/BioWare/Star Wars - The Old Republic/"
 
-# Oddness for reassigned PATH_TO_SWTOR_LAUNCHER comes from custom/express installation
-PREFIX_LOCATION=$(dirname "$(find ~/ -path \*drive_c/Program\ Files/Electronic\ Arts -print -quit 2>/dev/null| tail -1 | grep -o '.*/drive_c')")
-PATH_TO_SWTOR_LAUNCHER="/drive_c/Program Files/Electronic Arts/BioWare/Star Wars - The Old Republic/"
-cd "$PREFIX_LOCATION$PATH_TO_SWTOR_LAUNCHER" || PATH_TO_SWTOR_LAUNCHER="/drive_c/Program Files/Electronic Arts/BioWare/Star Wars-The Old Republic/"
-WINEPREFIX=$PREFIX_LOCATION wine "$PREFIX_LOCATION$PATH_TO_SWTOR_LAUNCHER/launcher.exe"
+WINEPREFIX=$PREFIX_DIR wine "$SWTOR_LAUNCHER_DIR/launcher.exe"
