0aba62329cb2606b7a84fae654aac871f2dd60e4
Bernard Potocki <bernard.potocki@imanel.org>
2022-02-22 21:35:55 +0100

    Compress SWTOR.app with prefix inside

--- package.sh
+++ package.sh
@@ -27,17 +27,55 @@ create_skeleton_app() {
   cp -v "$TEMPLATE_DIR/SWTOR.sh" "$APP_DIR/Contents/MacOS/SWTOR"
 }
 
+move_prefix_to_app() {
+  log "->\tMoving prefix to SWTOR.app"
+  mv -v "$PREFIX_DIR" "$APP_DIR/Contents/Resources/"
+}
+
+compress_app() {
+  local file_name="$1.tar.gz"
+  log "->\Compressing SWTOR.app as $file_name"
+  tar -cvzf "$DIST_DIR/$file_name" -C "$DIST_DIR" --gid 0 --uid 0 "SWTOR.app"
+}
+
+
+move_back_prefix_dir() {
+  log "->\tMoving prefix dir back to original place"
+  mv -v "$APP_DIR/Contents/Resources/prefix" "$PREFIX_DIR"
+}
+
+remove_skeleton_app() {
+  log "->\tRemoving skeleton SWTOR.app"
+  rm -rf "$APP_DIR"
+}
+
 package() {
 
+  log "_ _ _ _ _ _ _ _ _ _ _ _"
   log "Step 1: Create dist dir"
   log "‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾"
 
   create_dist_dir
 
+  log "_ _ _ _ _ _ _ _ _ _ _ _ _"
   log "Step 2: Create SWTOR.app"
-  log "‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾"
+  log "‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾"
 
   create_skeleton_app
+  move_prefix_to_app
+
+  log "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _"
+  log "Step 3: Prepare SWTOR-light package"
+  log "‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾"
+
+  compress_app "SWTOR-light"
+
+  log "_ _ _ _ _ _ _ _"
+  log "Step 9: Cleanup"
+  log "‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾"
+
+  move_back_prefix_dir
+  remove_skeleton_app
 
 }
 
